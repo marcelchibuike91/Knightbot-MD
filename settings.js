bbe110b03f7bd2469c029ec2eb2185276d95725c
marcelchibuike91 <marcelchibuike91@gmail.com>
2025-05-21 21:05:08 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'ZERAK⚔️'👽⚔️, // Your name
+  ownerNumber: '2347059083625', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
